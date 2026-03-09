1f77163acd1b1abd2d05d66dc442451cc076b87d
Chuning Zhu <chuning.zhu@gmail.com>
2026-03-09 07:34:28 +0000

    Update tensorflow version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
-        "tensorflow==2.15.0",
+        "tensorflow==2.20.0",
         "tensorflow_datasets>=4.9.2",
     ],
     extras_require={
